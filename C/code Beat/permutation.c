e27d31eba4801b4c4738383c7c162255c387161b
Lavish Aggarwal <lucky.lavish@gmail.com>
2014-11-02 17:50:39 +0530

    re-structured

--- C/code Beat/permutation.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int i;
-	char a[100];
-	scanf("%s",&a);
-
-}
--- C/unsignedTest.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-//typedef unsigned int uint;
-int main()
-{
-	unsigned int i;
-	scanf("%u",&i);
-	printf("%u\n",i);
-}
