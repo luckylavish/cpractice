ed46d1f9ac2efb26ac0156787f06b24b8de8fe94
Lavish Aggarwal <lucky.lavish@gmail.com>
2014-11-16 20:44:24 +0530

    code force compition

---
+++ C/Compititions/codeforce/round 277 div 2/calcuatingFunction.c
@@ -0,0 +1,49 @@
+/*A. Calculating Function
+time limit per test
+1 second
+memory limit per test
+256 megabytes
+input
+standard input
+output
+standard output
+
+For a positive integer n let's define a function f:
+
+f(n) =  - 1 + 2 - 3 + .. + ( - 1)nn
+
+Your task is to calculate f(n) for a given integer n.
+Input
+
+The single line contains the positive integer n (1 ≤ n ≤ 1015).
+Output
+
+Print f(n) in a single line.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+long long int calculation(long long int n);
+int main()
+{
+	long long int *n = (long long int *)malloc(sizeof(long long int));
+	scanf("%lld",n);
+	printf("%lld",calculation(*n));
+	return 0;
+}
+long long int calculation(long long int n)
+{	
+	int j=1;
+	long long int i,sum=0;
+	if(n == 0)
+		return 0;
+	else
+	{
+		for(i= 1;i<=n;i++)
+		{
+			j = (j*-1);
+			sum = sum + (j*i);
+		}
+	}
+	return sum;
+}
